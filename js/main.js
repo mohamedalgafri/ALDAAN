832c37df988dfeb7a7942051433a0685ceebc8e0
mohamedalgafri <mohamed18540m@gmail.com>
2022-08-09 16:57:21 -0700

    firstcommit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+////////////// swiper1 ///////////
+
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
