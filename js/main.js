49704c842fa24da6fe6f7049cb37e6ce5e604217
mohamedalgafri <mohamed18540m@gmail.com>
2022-08-10 01:49:18 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 
-////////////// swiper1 ///////////
+const nav_mnue = document.querySelector(".menu_nav"),
+      nav_but = document.querySelector(".icon_menu"),
+      nav_close = document.querySelector(".close_menu"),
+      body = document.querySelector(".body");
 
-var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+nav_but.addEventListener('click',()=>{
+  nav_mnue.classList.add("active");
+  nav_close.classList.add("active");
+  body.classList.add("active");
+})
+
+nav_close.addEventListener('click',()=>{
+  nav_mnue.classList.remove("active");
+  nav_close.classList.remove("active");
+  body.classList.remove("active");
+})
+
+
+var lastTop;
+
+function stopScrolling() {
+    lastTop = $(window).scrollTop();      
+    $('body').addClass( 'noscroll' )          
+         .css( { top: -lastTop } )        
+         ;            
+}
+
+function continueScrolling() {                    
+
+    $('body').removeClass( 'noscroll' );      
+    $(window).scrollTop( lastTop );       
+} 
