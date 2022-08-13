8083a287830e2cc34fc06b59d1241c3f7c7c23d9
mohamedalgafri <mohamed18540m@gmail.com>
2022-08-12 23:49:07 -0700

    up

--- js/artchit.js
+++ js/artchit.js
@@ -17,12 +17,12 @@
 
 /////////////////////////////////////////
 
-var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 10,
+//     slidesPerView: 4,
+//     freeMode: true,
+//     watchSlidesProgress: true,
+//   });
   var swiper2 = new Swiper(".mySwiper2", {
     spaceBetween: 10,
     navigation: {
